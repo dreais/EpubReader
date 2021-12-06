6d45207fe6b7d95847da41d88633e137a80a3430
dreais <valentin.lattay@gmail.com>
2021-12-06 21:05:42 +0100

    removing useless main stuff

--- Sources/main.c
+++ Sources/main.c
@@ -3,20 +3,13 @@
 int main(void)
 {
     Core core = {0};
-    HTMLNode *page;
-    core.epub = prepareEpub("caughtup.epub");
+//    HTMLNode *page;
+//    core.epub = prepareEpub("caughtup.epub"); // THOSE ARE HARDCODED VALUE, THIS IS FOR DEBUG PURPOSE
+//
+//    core.manifest = prepareManifest(&core.epub);
+//    core.toc = prepareTOC(&core.epub);
+//
+//    page = parseHTMLFile(read_zfile(core.epub.zipfile, core.manifest.hrefList[getIndexManifest(&core, "id267")]));
 
-    core.manifest = prepareManifest(&core.epub);
-    core.toc = prepareTOC(&core.epub);
-
-    page = parseHTMLFile(read_zfile(core.epub.zipfile, core.manifest.hrefList[getIndexManifest(&core, "id267")]));
-
-    for (int i = 0; page[i].tag; i++) {
-        if (page[i].content) {
-            printf("%s\n", page[i].content);
-        }
-    }
-
-    scanf("%s\n", NULL);
     return 0;
 }
